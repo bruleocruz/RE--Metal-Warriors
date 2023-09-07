fba0e51aa111b36c2da8638fe946c7f2c517cc1d
Bruno <bruleocruz@gmail.com>
2023-09-07 20:52:03 -0300

    Game and Editor class;
    Transição entre Main, Game e Editor criada, através de comandos;

--- editor.py
+++ editor.py
@@ -1,22 +1,22 @@
 import pygame as pg
 from config import *
 
-class Game:
+class Editor:
     def __init__(self, main):
         self.SCREEN = main.SCREEN
         self.main = main
-        self.game_screen = pg.Surface((800, 600))
+        self.editor_screen = pg.Surface((800, 600))
     
     def run(self):
-        self.game_screen.fill('Green')
+        self.editor_screen.fill('Blue')
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
                     if event.key == pg.K_m:  # Main Transition;
                         self.main.main_start = True
-                        self.main.game_start = False
-                    elif event.key == pg.K_e:  # Editor Transition;
-                         self.main.editor_start = True
-                         self.main.game_start = False
+                        self.main.editor_start = False
+                    elif event.key == pg.K_g:  # Game Transition;
+                         self.main.game_start = True
+                         self.main.editor_start = False
                     elif event.key == pg.K_ESCAPE:
                          self.main.playing = False
-        self.SCREEN.blit(self.game_screen, (0, 0))
+        self.SCREEN.blit(self.editor_screen, (0, 0))
--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 import pygame as pg
 from config import *
+from game import Game
+from editor import Editor
+
+# Main Screen = Red
+# Game Screen = Green
+# Editor Screen = Blue
 
 pg.init()
 
-main_screen = pg.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('RE: Metal Warriors')
 
 class Main:
-    def __init__(self, main_screen):
-        self.main_screen = main_screen
-        self.game_screen = pg.Surface((800, 600))
+    def __init__(self, SCREEN):
+        self.SCREEN = SCREEN
+        self.main_screen = pg.Surface((800, 600))
         self.fps = pg.time.Clock()
+
+        self.game = Game(self)
+        self.editor = Editor(self)
 
         self.playing = True
         self.main_start = True
+        self.game_start = False
+        self.editor_start = False
 
     def run(self):
         while self.playing:
             if self.main_start:
-                self.game_screen.fill('red')
+                self.main_screen.fill('red')
                 for event in pg.event.get():
                     if event.type == pg.QUIT:
                         self.main_start = False
                         self.playing = False
-                    if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-                        self.main_start = False
-                        self.playing = False
-        
-                self.main_screen.blit(self.game_screen, (0, 0))
+                    if event.type == pg.KEYDOWN:
+                        if event.key == pg.K_ESCAPE:
+                            self.main_start = False
+                            self.playing = False
+                        elif event.key == pg.K_g:  # Game Transition;
+                            self.game_start = True
+                            self.main_start = False
+                        elif event.key == pg.K_e:
+                            self.editor_start = True
+                            self.main_start = False
+                self.SCREEN.blit(self.main_screen, (0, 0))
+            
+            if self.game_start:
+                self.game.run()
+            
+            if self.editor_start:
+                self.editor.run()
 
             pg.display.update()
             self.fps.tick(60)
 
-main = Main(main_screen)
+main = Main(SCREEN)
 main.run()
